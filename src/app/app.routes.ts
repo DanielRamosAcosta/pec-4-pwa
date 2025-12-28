cfb3bcc9999e90e70fa9b4a9b7803dd5967bf241
Daniel Ramos <danielramosacosta1@gmail.com>
2025-12-28 22:08:49 +0000

    feat(routing): configure app routes with lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/recipes',
+    pathMatch: 'full'
+  },
+  {
+    path: 'recipes',
+    loadComponent: () => import('./pages/recipes-list/recipes-list.component').then(
+      m => m.RecipesListComponent
+    )
+  },
+  {
+    path: 'recipe/:id',
+    loadComponent: () => import('./pages/recipe-detail/recipe-detail.component').then(
+      m => m.RecipeDetailComponent
+    )
+  },
+  {
+    path: '**',
+    redirectTo: '/recipes'
+  }
+];
